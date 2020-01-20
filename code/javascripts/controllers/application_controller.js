f63a842d241566c3b55ee7dab91b245140e83db4
Rob Cameron <rob.cameron@fastmail.com>
2020-01-20 11:40:43 -0800

    Remove unused JS/CSS

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -3,16 +3,5 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  connect() {
-    console.log(`
-                    Welcome to
-            ┌─┐┌─┐┌┬┐┌─┐┬─┐┌─┐┌┐┌ ╦╔═╗
-            │  ├─┤│││├┤ ├┬┘│ ││││ ║╚═╗
-            └─┘┴ ┴┴ ┴└─┘┴└─└─┘┘└┘╚╝╚═╝
-
-                    Find me in
-code/javascripts/controllers/application_controller.js
-
-    `);
-  }
+  connect() {}
 }
