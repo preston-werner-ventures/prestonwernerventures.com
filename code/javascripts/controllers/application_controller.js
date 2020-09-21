7fb07ed2e28d5617b0564ebfd8fa0e948c426edf
Rob Cameron <rob.cameron@fastmail.com>
2020-09-21 15:27:11 -0700

    Adds twitter feed to homepage

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -4,14 +4,33 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static get targets() {
-    return ["year"];
+    return ["twitterStream","year"];
   }
 
   connect() {
     this._setYear();
+    this._showTwitterStream();
   }
 
   _setYear() {
     this.yearTarget.textContent = new Date().getFullYear();
+  }
+
+  _showTwitterStream() {
+    if (this.hasTwitterStreamTarget) {
+      twttr.widgets.createTimeline(
+        {
+          sourceType: "profile",
+          screenName: this.twitterStreamTarget.dataset['profile']
+        },
+        this.twitterStreamTarget,
+        {
+          chrome: "noheader nofooter",
+          tweetLimit: this.twitterStreamTarget.dataset['limit'],
+          borderColor: '#ffffff',
+          dnt: true
+        }
+      );
+    }
   }
 }
