333adaf7e3866b7ef445a64545e6430302c61389
Rob Cameron <cannikin@fastmail.com>
2022-11-08 14:07:02 -0800

    Update to Tailwind 3.2, use Tailwind CLI instead of PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("postcss-nested"),
-    require("autoprefixer"),
-    process.env.NODE_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: ["./publish/**/*.html"],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelist: ["tab-active"]
-      })
-  ]
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
